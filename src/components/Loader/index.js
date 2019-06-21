c517de52b3ea4de7be33a2cfb6fd9cf3aa857ff5
Carolina <carolinalaginestra@hotmail.com>
2019-06-20 23:01:44 -0300

    get all books and adjusting loader alt

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => { 
     return (
         <div className="loader-gif"> 
-            <img src={require('../../icons/loader.gif')} alt="loading..." />
+            <img src={require('../../icons/loader.gif')} alt="loading, please wait" />
         </div>
     )
 }
