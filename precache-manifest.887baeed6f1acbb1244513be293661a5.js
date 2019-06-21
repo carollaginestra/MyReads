ae2dcab4007d7366f5402b0a7fb2fc9ba2d2e641
Carolina <carolinalaginestra@hotmail.com>
2019-06-20 23:51:48 -0300

    Updates

---
+++ precache-manifest.887baeed6f1acbb1244513be293661a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "22975dd1ebc39cfc8a27906ddf528fcc",
+    "url": "/MyReads/static/media/no-cover.22975dd1.jpg"
+  },
+  {
+    "revision": "def46ae49109290f3b86a22e2d465ecc",
+    "url": "/MyReads/static/media/loader.def46ae4.gif"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "d1487915cb0126728201",
+    "url": "/MyReads/static/js/main.d1487915.chunk.js"
+  },
+  {
+    "revision": "c915d7aa2f202b3b451a",
+    "url": "/MyReads/static/js/1.c915d7aa.chunk.js"
+  },
+  {
+    "revision": "d1487915cb0126728201",
+    "url": "/MyReads/static/css/main.9329cbc3.chunk.css"
+  },
+  {
+    "revision": "82374d3095f6da1461625dc58dd42c82",
+    "url": "/MyReads/index.html"
+  }
+];
